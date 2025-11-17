15098b663c5a217b5545a30a43b84cd7fc25b110
terresteur <112128653+terresteur@users.noreply.github.com>
2025-11-17 12:55:34 -0500

    commentaire-AppHeader-AppFooter

--- src/components/common/AppFooter.vue
+++ src/components/common/AppFooter.vue
@@ -1,22 +1,24 @@
+<!--Le AppFooter seras appeler dans toutes les pages en bas.-->
 <template>
+  <!--Footer credite l'equipe derrieure le projet et il seras appeler dans toutes les pages en bas.-->
   <footer class="app-footer">
     <p>Fait par l'équipe voyageur® 2025</p>
   </footer>
 </template>
 
 <script>
-export default {
-  name: "AppFooter",
-};
+  export default {
+    name: "AppFooter",
+  };
 </script>
 
 <style scoped>
-.app-footer {
-  border-top: 1px solid #ccc;
-  text-align: center;
-  padding: 1rem 0;
-  font-size: 0.9rem;
-  color: #7a7a7a;
-  user-select: none;
-}
+  .app-footer {
+    border-top: 1px solid #ccc;
+    text-align: center;
+    padding: 1rem 0;
+    font-size: 0.9rem;
+    color: #7a7a7a;
+    user-select: none;
+  }
 </style>
