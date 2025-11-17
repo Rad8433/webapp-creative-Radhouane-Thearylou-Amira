362be831ecf17f8ee4e4b087244975f396b3d772
Radhouane <143246722+Rad8433@users.noreply.github.com>
2025-11-17 13:10:49 -0500

    carte filtrés fonctionnel

--- src/components/specific/MemorySwiper.vue
+++ src/components/specific/MemorySwiper.vue
@@ -1,42 +1,58 @@
 <template>
   <div>
-    <swiper :effect="'coverflow'" :grabCursor="true" :centeredSlides="true" :slidesPerView="'auto'" :spaceBetween="-40"
+    <swiper
+      :effect="'coverflow'"
+      :grabCursor="true"
+      :centeredSlides="true"
+      :slidesPerView="'auto'"
+      :spaceBetween="-40"
       :coverflowEffect="{
         rotate: 50,
         stretch: 0,
         depth: 60,
         modifier: 1,
         slideShadows: false
-      }" :pagination="false" :modules="modules" class="mySwiper">
+      }"
+      :pagination="false"
+      :modules="modules"
+      class="mySwiper"
+    >
       <swiper-slide v-for="(card, index) in filteredMemories" :key="index">
         <MemoryCard v-bind="card" />
       </swiper-slide>
     </swiper>
   </div>
 </template>
- 
+
 <script>
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import { EffectCoverflow, Pagination } from 'swiper/modules';
- 
+
 import MemoryCard from './MemoryCard.vue';
 import data from '@/data/memoires.json';
- 
+import { useMemoryStore } from '@/stores/useMemoryStore';
+
 export default {
   name: 'MemorySwiper',
   components: { Swiper, SwiperSlide, MemoryCard },
   props: {
-    filters: Object,  
+    filters: {
+      type: Object,
+      default: () => ({ search: '', tag: 'all', date: '', sort: '' })
+    },
   },
   data() {
     return {
       modules: [EffectCoverflow, Pagination],
+    
+      roomId: null,
+      room: null,
       memories: data.memoires.map((souvenir, index) => ({
         id: index,
-        roomId: souvenir["room-id"],
+        roomId: souvenir.roomId, // normalize to number
         title: souvenir.titreSouvenir,
         image: souvenir.imageSouvenir,
         caption: souvenir.descriptionSouvenir,
@@ -47,47 +63,63 @@ export default {
       }))
     };
   },
+  created() {
+    // Convert roomId from URL to number
+    this.roomId = this.$route.params.id;
+
+  },
   computed: {
     filteredMemories() {
       let filtered = this.memories;
- 
+    
+      // Filter by roomId
+      if (this.roomId !== null) {
+        filtered = filtered.filter(memory => memory.roomId === this.roomId);
+      }
+
+
+      // Filter by search
       if (this.filters.search) {
         filtered = filtered.filter(memory =>
           memory.title.toLowerCase().includes(this.filters.search.toLowerCase())
         );
       }
- 
-      if (this.filters.tag !== "all") {
+
+      // Filter by tag
+      if (this.filters.tag && this.filters.tag !== 'all') {
         filtered = filtered.filter(memory =>
           memory.tags.includes(this.filters.tag)
         );
       }
- 
-      if (this.filters.date === "recent") {
+
+      // Sort by date
+      if (this.filters.date === 'recent') {
         filtered = filtered.sort((a, b) => new Date(b.date) - new Date(a.date));
-      } else if (this.filters.date === "old") {
+      } else if (this.filters.date === 'old') {
         filtered = filtered.sort((a, b) => new Date(a.date) - new Date(b.date));
       }
- 
-      if (this.filters.sort === "asc") {
+
+      // Sort by memoryNumber
+      if (this.filters.sort === 'asc') {
         filtered = filtered.sort((a, b) => a.memoryNumber.localeCompare(b.memoryNumber));
-      } else if (this.filters.sort === "desc") {
+      } else if (this.filters.sort === 'desc') {
         filtered = filtered.sort((a, b) => b.memoryNumber.localeCompare(a.memoryNumber));
       }
- 
+
       return filtered;
     }
   }
 };
+
 </script>
- 
+
 <style scoped>
 .mySwiper {
   width: 100%;
   padding: 50px 0;
   margin: auto;
 }
- 
+
 .swiper-slide {
   display: flex;
   justify-content: center;
