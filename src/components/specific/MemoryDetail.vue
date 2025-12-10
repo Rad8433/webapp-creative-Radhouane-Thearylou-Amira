0221e98bbf1c11276ea5a0e36a49fe2b0a62ecad
Rad8433 <radhkordan19@gmail.com>
2025-12-10 12:27:06 -0500

    corrections

--- src/components/specific/MemoryDetail.vue
+++ src/components/specific/MemoryDetail.vue
@@ -5,7 +5,8 @@
         <div class="boutonDetail">
           <!-- Bouton retour -->
           <BaseButton variant="quatrieme" @click="goBack">
-            <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-label="Fleche du retoure en arriere d'une page.">
+            <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+              aria-label="Fleche du retoure en arriere d'une page.">
               <path fill-rule="evenodd" clip-rule="evenodd"
                 d="M11.7071 4.29289C12.0976 4.68342 12.0976 5.31658 11.7071 5.70711L6.41421 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H6.41421L11.7071 18.2929C12.0976 18.6834 12.0976 19.3166 11.7071 19.7071C11.3166 20.0976 10.6834 20.0976 10.2929 19.7071L3.29289 12.7071C3.10536 12.5196 3 12.2652 3 12C3 11.7348 3.10536 11.4804 3.29289 11.2929L10.2929 4.29289C10.6834 3.90237 11.3166 3.90237 11.7071 4.29289Z"
                 fill="#fff" />
@@ -34,7 +35,8 @@
           <!-- Image existante ou placeholder -->
           <div v-if="editableMemory?.image" class="image-click-area" :class="{ editable: isEditing }"
             @click="isEditing && triggerFileInput()">
-            <img :src="editableMemory.image" class="preview-image" alt="Image de la carte en mode edition." loading="lazy"/>
+            <img :src="editableMemory.image" class="preview-image" alt="Image de la carte en mode edition."
+              loading="lazy" />
           </div>
           <div v-else class="image readonly" :class="{ editable: isEditing }" @click="isEditing && triggerFileInput()">
             <span class="plus">+</span>
@@ -63,7 +65,7 @@
               <span class="select-arrow">▾</span>
             </div>
             <p v-else class="readonly">
-              {{ editableMemory.tags?.join(", ") || "Aucun tag" }}
+              {{ editableMemory.tag || "Aucun tag" }}
             </p>
           </div>
         </div>
@@ -145,7 +147,7 @@ export default {
     }
     // Initialise la mémoire éditable et le tag sélectionné
     this.editableMemory = { ...memory };
-    this.selectedTag = memory.tags?.[0] || "";
+    this.selectedTag = memory.tag || "";
   },
 
   methods: {
@@ -186,7 +188,7 @@ export default {
         caption: this.editableMemory.caption,
         date: this.editableMemory.date,
         image: this.editableMemory.image,
-        tags: this.selectedTag,
+        tag: this.selectedTag, // <-- singular
       });
     },
     // Demande de confirmation avant suppression
@@ -200,8 +202,7 @@ export default {
 };
 </script>
 <style scoped>
-
-/*Gérer l’accessibilité au clavier de la navigation.*/ 
+/*Gérer l’accessibilité au clavier de la navigation.*/
 /* Style pour la mise au point accessible */
 #tag-select:focus-visible {
   outline: 3px solid #0245ff;
@@ -210,8 +211,8 @@ export default {
 }
 
 .image-circle:focus-visible {
-  outline: solid 3px #0245ff;  
-  outline-offset: 4px;         
+  outline: solid 3px #0245ff;
+  outline-offset: 4px;
   border-radius: 6px;
 }
 
@@ -226,7 +227,7 @@ export default {
   width: 100%;
   max-width: 640px;
   box-sizing: border-box;
-  margin:0 auto;
+  margin: 0 auto;
 }
 
 /* Conteneur des contenus du formulaire */
@@ -282,7 +283,7 @@ export default {
   align-items: center;
   border: none;
   outline: none;
-  color:#1d1d1d;
+  color: #1d1d1d;
   box-sizing: border-box;
 }
 
